4e85f58e254b2ca13c62bf2a2a000657c09630e9
Henri Lunnikivi <heggggxa@gmail.com>
2022-12-02 12:40:26 +0200

    Add day 1

---
+++ examples/day1.rs
@@ -0,0 +1,44 @@
+const INPUT: &str = include_str!("inputs/day1.txt");
+
+fn main() -> anyhow::Result<()> {
+    let mut elves = vec![];
+
+    let mut lines = INPUT.lines();
+    let mut elf = vec![];
+    while let Some(line) = lines.next() {
+        if line.is_empty() {
+            // Store this elf and start building another one
+            elves.push(elf);
+            elf = vec![];
+            continue;
+        }
+
+        let num = line.parse::<u32>()?;
+        elf.push(num);
+    }
+
+    elves.sort_by(|a, b| a.iter().sum::<u32>().cmp(&b.iter().sum::<u32>()));
+
+    part1(&mut elves);
+    part2(&mut elves);
+
+    Ok(())
+}
+
+fn part1(elves: &mut [Vec<u32>]) {
+    let fattest = elves.last().unwrap();
+    let n = fattest.into_iter().sum::<u32>();
+
+    println!("{n}");
+}
+
+fn part2(elves: &mut [Vec<u32>]) {
+    let three_fattest = elves
+        .iter()
+        .rev()
+        .take(3)
+        .map(|x| x.iter().sum::<u32>())
+        .sum::<u32>();
+
+    println!("{three_fattest}");
+}
