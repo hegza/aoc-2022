b78eeb510433ccceb69401c04a92ca4670d6f058
Henri Lunnikivi <heggggxa@gmail.com>
2022-12-11 15:38:25 +0200

    Day 11 Part 2

--- examples/day11.rs
+++ examples/day11.rs
@@ -1,6 +1,5 @@
-use std::{num::ParseIntError, str::FromStr};
-
 use itertools::Itertools;
+use std::{num::ParseIntError, str::FromStr};
 
 const INPUT: &str = include_str!("inputs/day11.txt");
 impl std::str::FromStr for Monkey {
@@ -38,7 +37,7 @@ impl std::str::FromStr for Monkey {
             };
             (operator, param)
         };
-        let op_fn = move |old| {
+        let op = move |old: usize| {
             let param = match param {
                 Some(n) => n,
                 None => old,
@@ -49,7 +48,7 @@ impl std::str::FromStr for Monkey {
                 _ => panic!("unknown op: {}", operator),
             }
         };
-        let op = Box::new(op_fn);
+        let op = Box::new(op);
 
         let test_var = lines
             .next()
@@ -93,20 +92,26 @@ struct Monkey {
 
 impl Monkey {
     // Returns (where, what)
-    fn take_turn(&mut self) -> Vec<(usize, usize)> {
+    fn take_turn(&mut self, relief: bool, common_denom: usize) -> Vec<(usize, usize)> {
         let mut thrown = Vec::with_capacity(self.items.len());
         let items = self.items.clone();
         self.items.clear();
         for item in items {
-            thrown.push(self.inspect(item));
+            thrown.push(self.inspect(item, relief, common_denom));
         }
         thrown
     }
 
     // Returns thrown (where, what)
-    fn inspect(&self, item: usize) -> (usize, usize) {
+    fn inspect(&self, item: usize, relief: bool, common_denom: usize) -> (usize, usize) {
         let item = (self.op)(item);
-        let item = item / 3;
+
+        // Part 1 only
+        let item = if relief {
+            item / 3
+        } else {
+            item % common_denom
+        };
 
         let next = if (item % self.test_var) == 0 {
             self.next_if_true
@@ -117,13 +122,14 @@ impl Monkey {
     }
 }
 
-fn round(monkeys: &mut [Monkey]) -> [usize; 8] {
+fn round(monkeys: &mut [Monkey], relief: bool, divs: &[usize]) -> [usize; 8] {
     let mut inspections = [0; 8];
 
+    let common_denom: usize = divs.iter().product();
     for idx in 0..8 {
         let monkey = &mut monkeys[idx];
-        let thrown = monkey.take_turn();
-        inspections[idx] += thrown.len();
+        let thrown = monkey.take_turn(relief, common_denom);
+        inspections[idx] += (thrown.len() as usize);
         for (next, item) in thrown {
             monkeys[next].items.push(item);
         }
@@ -137,12 +143,35 @@ fn part1(mut monkeys: Vec<Monkey>) -> usize {
 
     let mut inspections = vec![0; 8];
     for _ in 0..20 {
-        let this_round = round(monkeys);
+        let this_round = round(monkeys, true, &[]);
         this_round
             .iter()
             .zip(inspections.iter_mut())
             .for_each(|(round, i)| {
-                *i += round;
+                *i += *round;
+            });
+    }
+
+    inspections.sort_by(|a, b| b.cmp(a));
+    let two_most_active = &inspections[0..2];
+
+    // Monkey business
+    two_most_active[0] * two_most_active[1]
+}
+
+fn part2(mut monkeys: Vec<Monkey>) -> usize {
+    let divisors = monkeys.iter().map(|m| m.test_var).collect_vec();
+
+    let monkeys = monkeys.as_mut_slice();
+
+    let mut inspections = vec![0; 8];
+    for _rnd in 0..10_000 {
+        let this_round = round(monkeys, false, &divisors);
+        this_round
+            .iter()
+            .zip(inspections.iter_mut())
+            .for_each(|(round, i)| {
+                *i += *round;
             });
     }
 
@@ -159,7 +188,7 @@ fn main() -> anyhow::Result<()> {
         .map(|text| Monkey::from_str(text).unwrap())
         .collect_vec();
 
-    println!("Part 1: {}", part1(monkeys));
-
+    //println!("Part 1: {}", part1(monkeys));
+    println!("Part 2: {}", part2(monkeys));
     Ok(())
 }
