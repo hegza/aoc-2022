c9409d3b7976e08491b02381ea3cf733e0472a18
Henri Lunnikivi <heggggxa@gmail.com>
2022-12-17 16:43:58 +0200

    Attempt to thoroughly optimize D17
    - Format profile output
    - Make layout into fixed size table
    - Short circuit collide with y-coord
    - Tabulate block height
    - Use tabulated for collide_* as well
    - Use only latest block for determining new height
    - Optimize the optimizer
    - Optimze less often
    - Add a hash for pos
    - Tabulate block width
    - Optimize a bit more often
    - Move vec instead of retain
    - Use block map instead of block list
    - Don't allocate as_coords
    - Move floor
    - Skip collisions to blocks when close to spawn
    - Tabulate profiles
    - More efficient as_coords

--- examples/day17.rs
+++ examples/day17.rs
@@ -1,5 +1,6 @@
 use itertools::Itertools;
 use std::{
+    collections::HashMap,
     ops,
     time::{Duration, SystemTime},
 };
@@ -8,7 +9,7 @@ const INPUT: &str = include_str!("inputs/day17.txt");
 
 const WIDTH: usize = 7;
 
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, Eq, Hash, PartialEq)]
 enum BlockKind {
     Dash,
     Plus,
@@ -17,12 +18,18 @@ enum BlockKind {
     Square,
 }
 
-#[derive(Clone, Copy, PartialEq)]
-struct Pos(usize, usize);
+#[derive(Clone, Copy, Debug)]
+struct Pos(usize, usize, usize);
+
+impl PartialEq for Pos {
+    fn eq(&self, other: &Self) -> bool {
+        self.2 == other.2
+    }
+}
 
 impl From<(usize, usize)> for Pos {
     fn from(xy: (usize, usize)) -> Self {
-        Self(xy.0, xy.1)
+        Self(xy.0, xy.1, xy.1 * WIDTH + xy.0)
     }
 }
 
@@ -32,34 +39,103 @@ impl From<Pos> for (usize, usize) {
     }
 }
 
-#[derive(Clone)]
-struct Block {
-    pos: Pos,
-    // Layout is indexed from bot-left to top-right
-    layout: Vec<Pos>,
-}
-
 impl ops::Add for Pos {
     type Output = Pos;
 
     fn add(self, rhs: Self) -> Self::Output {
-        Pos(self.0 + rhs.0, self.1 + rhs.1)
+        let x = self.0 + rhs.0;
+        let y = self.1 + rhs.1;
+        let hash = y * WIDTH + x;
+        Pos(x, y, hash)
     }
+}
+
+#[derive(Clone)]
+struct Block {
+    pos: Pos,
+    kind: BlockKind,
+    // Layout is indexed from bot-left to top-right
+    layout: [Pos; 7],
+    width: usize,
+    height: usize,
+}
+
+lazy_static::lazy_static! {
+    static ref LAYOUTS: HashMap<BlockKind, Vec<Pos>> = [
+            (BlockKind::Dash, [(0, 0), (1, 0), (2, 0), (3, 0), (0, 0), (0, 0), (0, 0)]),
+            (BlockKind::Plus, [(1, 0), (0, 1), (1, 1), (2, 1), (1, 2), (1, 0), (1, 0)]),
+            (BlockKind::J, [(0, 0), (1, 0), (2, 0), (2, 1), (2, 2), (0, 0), (0, 0)]),
+            (BlockKind::I, [(0, 0), (0, 1), (0, 2), (0, 3), (0, 0), (0, 0), (0, 0)]),
+            (BlockKind::Square, [(0, 0), (1, 0), (0, 1), (1, 1), (0, 0), (0, 0), (0, 0)]),
+    ]
+        .into_iter()
+        .map(|(bk, x)| (bk, x.into_iter().map(|x| x.into()).collect_vec()))
+        .collect();
+    static ref LEFT_PROFILE: HashMap<BlockKind, Vec<Pos>> = [
+            (BlockKind::Dash, vec![(0, 0)]),
+            (BlockKind::Plus, vec![(1, 0), (0, 1)]),
+            (BlockKind::J, vec![(0, 0), (2, 1), (2, 2)]),
+            (BlockKind::I, vec![(0, 0), (0, 1), (0, 2), (0, 3)]),
+            (BlockKind::Square, vec![(0, 0), (0, 1)]),
+    ]
+        .into_iter()
+        .map(|(bk, x)| (bk, x.into_iter().map(|x| x.into()).collect_vec()))
+        .collect();
+    static ref RIGHT_PROFILE: HashMap<BlockKind, Vec<Pos>> = [
+            (BlockKind::Dash, vec![(3, 0)]),
+            (BlockKind::Plus, vec![(1, 0), (2, 1)]),
+            (BlockKind::J, vec![(2, 0), (2, 1), (2, 2)]),
+            (BlockKind::I, vec![(0, 0), (0, 1), (0, 2), (0, 3)]),
+            (BlockKind::Square, vec![(1, 0), (1, 1)]),
+    ]
+        .into_iter()
+        .map(|(bk, x)| (bk, x.into_iter().map(|x| x.into()).collect_vec()))
+        .collect();
+    static ref BOT_PROFILE: HashMap<BlockKind, Vec<Pos>> = [
+            (BlockKind::Dash, vec![(0, 0), (1, 0), (2, 0), (3, 0)]),
+            (BlockKind::Plus, vec![(0, 1), (1, 0), (2, 1)]),
+            (BlockKind::J, vec![(0, 0), (1, 0), (2, 0)]),
+            (BlockKind::I, vec![(0, 0)]),
+            (BlockKind::Square, vec![(0, 0), (1, 0)]),
+    ]
+        .into_iter()
+        .map(|(bk, x)| (bk, x.into_iter().map(|x| x.into()).collect_vec()))
+        .collect();
 }
 
 impl Block {
     fn new(pos: Pos, kind: BlockKind) -> Self {
         let layout = match kind {
-            BlockKind::Dash => vec![(0, 0), (1, 0), (2, 0), (3, 0)],
-            BlockKind::Plus => vec![(1, 0), (0, 1), (1, 1), (2, 1), (1, 2)],
-            BlockKind::J => vec![(0, 0), (1, 0), (2, 0), (2, 1), (2, 2)],
-            BlockKind::I => vec![(0, 0), (0, 1), (0, 2), (0, 3)],
-            BlockKind::Square => vec![(0, 0), (0, 1), (1, 0), (1, 1)],
+            BlockKind::Dash => [(0, 0), (1, 0), (2, 0), (3, 0), (0, 0), (0, 0), (0, 0)],
+            BlockKind::Plus => [(1, 0), (0, 1), (1, 1), (2, 1), (1, 2), (1, 0), (1, 0)],
+            BlockKind::J => [(0, 0), (1, 0), (2, 0), (2, 1), (2, 2), (0, 0), (0, 0)],
+            BlockKind::I => [(0, 0), (0, 1), (0, 2), (0, 3), (0, 0), (0, 0), (0, 0)],
+            BlockKind::Square => [(0, 0), (1, 0), (0, 1), (1, 1), (0, 0), (0, 0), (0, 0)],
         }
-        .into_iter()
-        .map(|xy| xy.into())
-        .collect_vec();
-        Self { pos, layout }
+        .map(|xy| xy.into());
+
+        let height = match kind {
+            BlockKind::Dash => 1,
+            BlockKind::Plus => 3,
+            BlockKind::J => 3,
+            BlockKind::I => 4,
+            BlockKind::Square => 2,
+        };
+        let width = match kind {
+            BlockKind::Dash => 4,
+            BlockKind::Plus => 3,
+            BlockKind::J => 3,
+            BlockKind::I => 1,
+            BlockKind::Square => 2,
+        };
+
+        Self {
+            pos,
+            layout,
+            width,
+            height,
+            kind,
+        }
     }
     fn with_pos(&self, pos: Pos) -> Self {
         Block {
@@ -69,14 +145,23 @@ impl Block {
     }
     /// Returns the y of the square above this block
     fn above(&self) -> usize {
-        self.layout.iter().map(|pos| pos.1).max().unwrap() + self.pos.1 + 1
-    }
-    fn top(&self) -> usize {
-        self.layout.iter().map(|pos| pos.1).max().unwrap() + self.pos.1
+        self.pos.1 + self.height
     }
     /// Returns the x of the square right of this block
     fn to_right(&self) -> usize {
-        self.layout.iter().map(|pos| pos.0).max().unwrap() + self.pos.0 + 1
+        self.pos.0 + self.width
+    }
+    /// Returns the topmost y of this block
+    fn top(&self) -> usize {
+        self.pos.1 + self.height - 1
+    }
+    /// Returns the bottom-most y of this block
+    fn bottom(&self) -> usize {
+        self.pos.1
+    }
+    /// Returns the rightmost x of this block
+    fn right(&self) -> usize {
+        self.pos.0 + self.width - 1
     }
     fn draw(&self, draw_map: &mut Vec<Vec<bool>>) {
         for (x, y) in self.layout.iter().map(|pos| (self.pos + *pos).into()) {
@@ -84,7 +169,39 @@ impl Block {
         }
     }
 
+    fn as_coords(&self) -> impl Iterator<Item = (usize, usize)> + '_ {
+        self.layout
+            .iter()
+            .map(|pos| (pos.0 + self.pos.0, pos.1 + self.pos.1))
+    }
+    fn as_coords_at_pos(
+        &self,
+        at_pos: (usize, usize),
+    ) -> impl Iterator<Item = (usize, usize)> + '_ {
+        self.layout
+            .iter()
+            .map(move |pos| (pos.0 + at_pos.0, pos.1 + at_pos.1))
+    }
+    fn as_coords_bot(&self) -> impl Iterator<Item = (usize, usize)> + '_ {
+        BOT_PROFILE[&self.kind]
+            .iter()
+            .map(|pos| (pos.0 + self.pos.0, pos.1 + self.pos.1))
+    }
+    fn as_coords_left(&self) -> impl Iterator<Item = (usize, usize)> + '_ {
+        LEFT_PROFILE[&self.kind]
+            .iter()
+            .map(|pos| (pos.0 + self.pos.0, pos.1 + self.pos.1))
+    }
+    fn as_coords_right(&self) -> impl Iterator<Item = (usize, usize)> + '_ {
+        RIGHT_PROFILE[&self.kind]
+            .iter()
+            .map(|pos| (pos.0 + self.pos.0, pos.1 + self.pos.1))
+    }
     fn collide_point(&self, pos: &Pos) -> bool {
+        // Short circuit if y-distance is too much
+        if pos.1 > self.top() || pos.0 > self.right() {
+            return false;
+        }
         for spos in self.layout.iter().map(|pos| *pos + self.pos) {
             if &spos == pos {
                 return true;
@@ -92,10 +209,15 @@ impl Block {
         }
         false
     }
+
     /// Check for any overlap
-    fn collide(&self, other: &Block) -> bool {
+    fn collide_block(&self, newer: &Block) -> bool {
+        // Short circuit if y-distance is too much
+        if newer.pos.1 > self.top() || newer.pos.0 > self.right() {
+            return false;
+        }
         for spos in self.layout.iter().map(|pos| *pos + self.pos) {
-            for opos in other.layout.iter().map(|pos| *pos + other.pos) {
+            for opos in newer.layout.iter().map(|pos| *pos + newer.pos) {
                 if spos == opos {
                     return true;
                 }
@@ -105,8 +227,13 @@ impl Block {
     }
 
     // Returns true if the block came to rest
-    fn tick(&mut self, push_right: bool, map: &[Block], floor_h: usize) -> bool {
-        let Pos(x, y) = self.pos;
+    fn tick_with_full_collision(
+        &mut self,
+        push_right: bool,
+        map: &[Block],
+        floor_h: usize,
+    ) -> bool {
+        let Pos(x, y, _) = self.pos;
 
         // 1. Push (right or left)
         let nx = if push_right && self.to_right() < WIDTH {
@@ -124,13 +251,13 @@ impl Block {
                 // Reverse iterate for efficiency because top-most blocks are...
                 // at the top of the stack
                 .rev()
-                .any(|b| b.collide(&self.with_pos((nx, y).into())))
+                .any(|b| b.collide_block(&self.with_pos((nx, y).into())))
             {
                 self.pos.0 = nx;
             }
         }
 
-        let Pos(x, y) = self.pos;
+        let Pos(x, y, _) = self.pos;
 
         // 2. Fall
         if y == floor_h {
@@ -145,8 +272,49 @@ impl Block {
             // Reverse iterate for efficiency because top-most blocks are...
             // at the top of the stack
             .rev()
-            .any(|b| b.collide(&self.with_pos((x, ny).into())))
+            .any(|b| b.collide_block(&self.with_pos((x, ny).into())))
         {
+            self.pos.1 = ny;
+            false
+        }
+        // There would be a collision, so block came to rest
+        else {
+            true
+        }
+    }
+
+    // Returns true if the block came to rest
+    fn tick_optimized(&mut self, push_right: bool, map: &[[bool; 7]], skip_coll: bool) -> bool {
+        let Pos(x, y, _) = self.pos;
+
+        // 1. Push (right or left)
+        let nx = if push_right && self.to_right() < WIDTH {
+            Some(x + 1)
+        } else if !push_right && x != 0 {
+            Some(x - 1)
+        } else {
+            None
+        };
+
+        if let Some(nx) = nx {
+            // If there is no collision, update x
+            if skip_coll || !self.as_coords_at_pos((nx, y)).any(|co| map[co.1][co.0]) {
+                self.pos.0 = nx;
+            }
+        }
+
+        let Pos(x, y, _) = self.pos;
+
+        // 2. Fall
+        if y == 0 {
+            // If the block is already on the floor, it came to rest
+            return true;
+        }
+
+        let ny = y - 1;
+
+        // If there is no collision, update y
+        if !skip_coll || !self.as_coords_at_pos((x, ny)).any(|co| map[co.1][co.0]) {
             self.pos.1 = ny;
             false
         }
@@ -197,17 +365,15 @@ fn part1(
     for _ in 0..NUM_BLOCKS {
         let mut block = Block::new((2, height + 3).into(), block_order.next().unwrap());
         while let Some(push_dir) = push_dirs.next() {
-            if block.tick(push_dir, &blocks, 0) {
+            if block.tick_with_full_collision(push_dir, &blocks, 0) {
                 break;
             }
         }
+        // Update height at end of round
+        if block.above() > height {
+            height = block.above();
+        }
         blocks.push(block);
-        height = blocks
-            .iter()
-            .map(|b: &Block| b.above())
-            .max()
-            .unwrap_or(0)
-            .max(height);
         floor = blocks
             .iter()
             .map(|b| b.pos.1)
@@ -218,74 +384,71 @@ fn part1(
     height
 }
 
-/// Returns the number of optimized blocks
-fn optimize(blocks: &mut Vec<Block>, floor: usize) -> usize {
-    let prelen = blocks.len();
-    blocks.retain(|block| block.pos.1 >= floor);
-    let postlen = blocks.len();
-
-    /*
-    if prelen != postlen {
-        println!("Optimized {} of {}", prelen - postlen, prelen);
+fn freeze(block_map: &mut Vec<[bool; 7]>, block: &Block) {
+    for co in block.as_coords() {
+        block_map[co.1][co.0] = true;
     }
-    */
-
-    prelen - postlen
 }
 
 fn part2(
     mut push_dirs: impl Iterator<Item = bool>,
     mut block_order: impl Iterator<Item = BlockKind>,
 ) -> usize {
-    let mut blocks = vec![];
+    let mut block_map = vec![[false; WIDTH]; 1024];
 
     let mut height = 0;
     let mut floor = 0;
+    let mut score = 0;
 
     let mut pr = 0;
     let mut pt = SystemTime::now();
-    const NUM_BLOCKS: usize = 1000000000000usize;
+    const NUM_BLOCKS: usize = 1_000_000_000_000usize;
     for round in 0..NUM_BLOCKS {
         let since_last_measure = SystemTime::now().duration_since(pt).unwrap();
         if since_last_measure >= Duration::from_secs(1) {
             let rps = round - pr;
             println!(
-                "Blocks per second: {} ({:.0} minutes remaining)",
+                "Blocks per second: {} ({} seconds remaining)",
                 rps,
-                ((NUM_BLOCKS - round) / rps) as f64 / 60.0
+                ((NUM_BLOCKS - round) / rps)
             );
             pr = round;
             pt = SystemTime::now();
         }
 
         let mut block = Block::new((2, height + 3).into(), block_order.next().unwrap());
+
         while let Some(push_dir) = push_dirs.next() {
-            if block.tick(push_dir, &blocks, 0) {
+            if block.tick_optimized(push_dir, &block_map, block.pos.1 > height) {
                 break;
             }
         }
-        blocks.push(block);
-
         // Update height at end of round
-        height = blocks.iter().map(|b: &Block| b.above()).max().unwrap_or(0);
-        floor = blocks.iter().map(|b| b.pos.1).min().unwrap_or(floor);
+        if block.above() > height {
+            height = block.above();
+        }
+        freeze(&mut block_map, &block);
 
-        // Lift floor
-        if round % 100 == 0 {
-            if let Some(f) = (floor..height)
-                .find(|&y| (0..WIDTH).all(|x| any_collision(&(x, y).into(), &blocks)))
-            {
-                floor = f;
+        // Extend the block map if necessary
+        if block.above() + 10 > block_map.len() {
+            // Find a new floor
+            for y in (0..block.pos.1).rev() {
+                if block_map[y].iter().all(|b| *b) {
+                    floor = y;
+                    height -= y;
+                    score += y;
+                    break;
+                }
             }
-            optimize(&mut blocks, floor);
+            let nmap = block_map[floor..].iter().chain(
+                std::iter::once(&[false; WIDTH])
+                    .cycle()
+                    .take(block_map.len() - floor),
+            );
+            block_map = nmap.cloned().collect();
         }
-        /*
-        if round % (NUM_BLOCKS / 100) == 0 {
-            println!("Progress: {} %", round / (NUM_BLOCKS / 100));
-        }
-        */
     }
-    height
+    score + height
 }
 
 fn main() -> anyhow::Result<()> {
